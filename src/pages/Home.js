9dd5ac7b84d7ee1282b78f402bdd5500e0d38dbd
Maaz Junior <goalmaaz@gmail.com>
2022-08-02 23:34:32 +0100

    ss

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const Home = () => {
-    const [homeDetails, setHomeDetails] = useState([])
 
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'dc811aafffmsh7cf2c61db109937p1ffcfajsne527db6bea89',
-            'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
-        }
-    };
-    
-    fetch('https://hotels4.p.rapidapi.com/properties/list?destinationId=1506246&pageNumber=1&pageSize=25&checkIn=2020-01-08&checkOut=2020-01-15&adults1=1&sortOrder=PRICE&locale=en_US&currency=USD', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-    
-        return(
-            <div>
-                {setHomeDetails.map((item) => {
-                    return(
-                        <div>
-                            {item}
-                        </div>
-                    )
-                })}
-            </div>
-        )
+    return(
+        
+        <div>
+            <h1>Hello Africa</h1>
+        </div>
+    )
 }
 
 export default Home;
