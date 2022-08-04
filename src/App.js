ec648b62c5e687e15786db4ec179e5419073b0d6
Maaz Junior <goalmaaz@gmail.com>
2022-08-04 10:02:34 +0100

    s

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-     <Home />
+
+      <h1>HELLO WORLD</h1>
+      
     </div>
   );
 }
