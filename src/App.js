2e0ac5d8a4ed0dac093c40aa8fe0f6ea584d5b03
Maaz Junior <goalmaaz@gmail.com>
2022-08-04 22:10:16 +0100

    react navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-
-      <h1>HELLO WORLD</h1>
-
+      <Navbar />
     </div>
   );
 }
